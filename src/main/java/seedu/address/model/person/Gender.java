3d915718e5df352d5ed589f84c508064b67f878b
JadeCheah <tszskygypei2522@gmail.com>
2025-03-12 14:58:22 +0800

    Add Gender class

---
+++ src/main/java/seedu/address/model/person/Gender.java
@@ -0,0 +1,67 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's gender in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidGender(String)}
+ */
+public class Gender {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Gender should only be either 'm' or 'f', and it should not be blank";
+
+    /*
+     * The first character of the gender must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[mf]";
+
+    public final String gender;
+
+    /**
+     * Constructs a {@code Gender}.
+     *
+     * @param gender A valid gender.
+     */
+    public Gender(String gender) {
+        requireNonNull(gender);
+        checkArgument(isValidGender(gender), MESSAGE_CONSTRAINTS);
+        this.gender = gender;
+    }
+
+    /**
+     * Returns true if a given string is a valid gender.
+     */
+    public static boolean isValidGender(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return gender;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Gender)) {
+            return false;
+        }
+
+        Gender otherGender = (Gender) other;
+        return gender.equals(otherGender.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return gender.hashCode();
+    }
+
+}
