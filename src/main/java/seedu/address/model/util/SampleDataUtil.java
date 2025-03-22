ad28e3a49d9e087a33e70820ea1e07048e7a6589
Yu Sheng <kohyusheng@gmail.com>
2025-03-23 01:13:13 +0800

    Refactor default data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,31 +28,31 @@ public class SampleDataUtil {
 
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Gender("m"), new Height("1.7"), new Weight("70"),
+            new Person(new Name("Alex Yeoh"), new Gender("m"), new Height("1.75"), new Weight("70"),
                     new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Gender("m"), new Height("1.7"), new Weight("70"),
+                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("peanuts")),
+            new Person(new Name("Bernice Yu"), new Gender("f"), new Height("1.65"), new Weight("55"),
                     new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK,
-                    getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Gender("m"), new Height("1.7"), new Weight("70"),
+                    new Priority("low"), new MeetingDate("2025-10-15"), EMPTY_REMARK,
+                    getTagSet("milk", "eggs")),
+            new Person(new Name("Charlotte Oliveiro"), new Gender("f"), new Height("1.68"), new Weight("60"),
                     new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Gender("m"), new Height("1.7"), new Weight("70"),
+                    new Priority("low"), new MeetingDate("2025-11-22"), EMPTY_REMARK, getTagSet("wheat")),
+            new Person(new Name("David Li"), new Gender("m"), new Height("1.8"), new Weight("82"),
                     new Phone("91031282"), new Email("lidavid@example.com"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Gender("m"), new Height("1.7"), new Weight("70"),
+                    new Priority("low"), new MeetingDate("2025-09-10"), EMPTY_REMARK, getTagSet("fish")),
+            new Person(new Name("Irfan Ibrahim"), new Gender("m"), new Height("1.78"), new Weight("75"),
                     new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Gender("m"), new Height("1.7"), new Weight("70"),
+                    new Priority("low"), new MeetingDate("2025-10-28"), EMPTY_REMARK, getTagSet("peanuts")),
+            new Person(new Name("Roy Balakrishnan"), new Gender("m"), new Height("1.72"), new Weight("68"),
                     new Phone("92624417"), new Email("royb@example.com"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"), new Diet("low sodium"),
-                    new Priority("low"), new MeetingDate("2025-12-12"), EMPTY_REMARK, getTagSet("colleagues"))
+                    new Priority("low"), new MeetingDate("2025-12-02"), EMPTY_REMARK, getTagSet("fish"))
         };
     }
 
